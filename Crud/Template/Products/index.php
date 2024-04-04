c353f7881fc1f40678b9569187013b3f1928930a
Gabriel Candeias Barbosa <gabriel.barbosa.03@edu.unipar.br>
2024-04-03 23:36:44 -0300

    Fix(Products): product validation index

--- Crud/Template/Products/index.php
+++ Crud/Template/Products/index.php
@@ -27,55 +27,57 @@ require_once("../Components/headerIndex.html");
 
         await renderIndex("Products", "products-table", ["id", "name", "price", "supplier_id"]);
 
-        document.querySelector('.option').addEventListener("change", async () => {
+        if (document.querySelector('.option') != null) {
+            document.querySelector('.option').addEventListener("change", async () => {
 
-            let options = document.querySelectorAll('.option:checked')
+                let options = document.querySelectorAll('.option:checked')
 
-            if (options.length > 0) {
+                if (options.length > 0) {
 
-                document.getElementById("add-cart").style.display = "inline";
-            } else {
-
-                document.getElementById("add-cart").style.display = "none";
-            }
-        })
-
-        document.getElementById("add-cart").addEventListener('click', async () => {
-
-            let products = [];
-
-            let options = document.querySelectorAll('.option:checked')
-
-            if (options.length > 0) {
-
-                options.forEach((e) => {
-
-                    products.push(e.id)
-                })
-
-                let user = await JSON.parse(localStorage.getItem('user'));
-
-                if (user.id == null || user.id == undefined) {
-
-                    logout()
+                    document.getElementById("add-cart").style.display = "inline";
                 } else {
 
-
-                    let response = await addToCart({products: products, user_id: user.id});
-
-                    if (response['success'] == true) {
-
-                        options.forEach((e) => {
-
-                            e.checked = false
-                        })
-                        document.getElementById("add-cart").style.display = "none";
-                    }
+                    document.getElementById("add-cart").style.display = "none";
                 }
-            } else {
+            })
 
-                document.getElementById("add-cart").style.display = "none";
-            }
-        })
+            document.getElementById("add-cart").addEventListener('click', async () => {
+
+                let products = [];
+
+                let options = document.querySelectorAll('.option:checked')
+
+                if (options.length > 0) {
+
+                    options.forEach((e) => {
+
+                        products.push(e.id)
+                    })
+
+                    let user = await JSON.parse(localStorage.getItem('user'));
+
+                    if (user.id == null || user.id == undefined) {
+
+                        logout()
+                    } else {
+
+
+                        let response = await addToCart({products: products, user_id: user.id});
+
+                        if (response['success'] == true) {
+
+                            options.forEach((e) => {
+
+                                e.checked = false
+                            })
+                            document.getElementById("add-cart").style.display = "none";
+                        }
+                    }
+                } else {
+
+                    document.getElementById("add-cart").style.display = "none";
+                }
+            })
+        }
     })
 </script>
